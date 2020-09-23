408c0709abc058f6a07002d9423abe6746ab12b1
huangxu <huangxu@itcast.cn>
2020-09-23 18:37:40 +0800

    feat: react项目初始化

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FastClick from 'fastclick'
+import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader'
+import store from './store/index'
+import './config/rem'
+import Route from './router'
+import './style/base.scss'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+FastClick.attach(document.body)
+
+const render = Component => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <AppContainer>
+        <Component />
+      </AppContainer>
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render(Route)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+export default class RouteConfig extends Component {
+  render () {
+    return (
+      <HashRouter>
+        <Switch>
+          
+        </Switch>
+      </HashRouter>
+    )
+  }
+}
